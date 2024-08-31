7eafa7983990627819839a8836f78e6f86732610
hakeem-jc <hakeem_clarke14@yahoo.com>
2024-08-31 11:59:14 -0500

    feat: Add description section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Header from "./components/Header";
 
-
 export default function Home() {
   return (
-    <main className="dark:bg-gray-900 min-h-screen">
+    <main className="dark:bg-gray-900 text-white w-screen">
       <Header />
-      <section className="flex px-8 py-10">
-        <div className="mr-auto place-self-center lg:col-span-7">
+      <section className="flex px-8 py-10 h-screen">
+        <div className="mr-auto place-self-center ">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
             Fast Loans for Fair Prices
           </h1>
@@ -27,8 +26,25 @@ export default function Home() {
           />
         </div>
       </section>
+
+      <section className="flex justify-center items-center h-screen">
+        <div className="text-center">
+          <h1 className=" text-5xl mb-10">Ready to take the next step?</h1>
+          <p className="text-xl mb-8">
+            Get thousands in your bank in minutes with up 3 months to repay with
+            no hidden fees ever
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Get Started
+          </a>
+        </div>
+      </section>
+
       <footer className="p-6 border-t border-gray-600 w-screen text-center">
-        <p className="text-white">Lone Inc</p>
+        <p>Lone Inc</p>
       </footer>
     </main>
   );
