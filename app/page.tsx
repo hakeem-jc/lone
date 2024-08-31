11c6bdfd8cd7f91d54017dbf454405030e26f8f2
hakeem-jc <hakeem_clarke14@yahoo.com>
2024-08-31 12:05:31 -0500

    feat: Add footer

--- app/page.tsx
+++ app/page.tsx
@@ -43,8 +43,36 @@ export default function Home() {
         </div>
       </section>
 
-      <footer className="p-6 border-t border-gray-600 w-screen text-center">
-        <p>Lone Inc</p>
+      <footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="./" className="hover:underline">
+          Lone Inc™
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
       </footer>
     </main>
   );
